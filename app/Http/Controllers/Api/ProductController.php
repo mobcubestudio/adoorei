38fa41b9f761f56802033fc3942023e02f3c2983
Irwing <irwing@letsevolve.com.br>
2022-11-16 20:32:15 -0400

    Swagger

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -8,6 +8,229 @@ use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Adoorei",
+ *     description="Documentação com todos os métodos de acesso à API da prova de avaliação",
+ *     @OA\Contact(
+ *          email="irwingcg@gmail.com"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *          url="http://localhost:8000"
+ *      )
+ *
+ * @OA\Get(
+ *     path="/api/products",
+ *     tags={"Produtos"},
+ *     summary="Listar todos os produtos",
+ *     @OA\Response(
+ *          response="200",
+ *          description="Ok"
+ *      )
+ * )
+ *
+ * @OA\Post(
+ *     path="/api/products",
+ *     tags={"Produtos"},
+ *     summary="Adicona um novo produto",
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="name",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="price",
+ *                     type="float"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="description",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="category",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="image_url",
+ *                     type="url"
+ *                 ),
+ *                 example={"name": "Blusa Feminina", "price": 15.85, "description": "Blusa Feminina tamanho G", "category": "Roupas", "image_url": "http://site.com/imagem.jpg"}
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ *
+ * @OA\Put(
+ *     path="/api/products/{id}",
+ *     tags={"Produtos"},
+ *     summary="Editar um produto",
+ *     @OA\Parameter(
+ *         description="ID do produto",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         @OA\Schema(type="integer"),
+ *     ),
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="name",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="price",
+ *                     type="float"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="description",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="category",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="image_url",
+ *                     type="url"
+ *                 ),
+ *                 example={"name": "Blusa Masculina", "price": 35.50, "description": "Blusa Masculina tamanho P", "category": "Roupas", "image_url": "http://site.com/imagem.jpg"}
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Produto Inexistente"
+ *     )
+ * )
+ *
+ * @OA\Delete(
+ *     path="/api/products/{id}",
+ *     tags={"Produtos"},
+ *     summary="Excluir um produto",
+ *     @OA\Parameter(
+ *         description="ID do produto",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         @OA\Schema(type="integer"),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Produto Inexistente"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/products/{id}",
+ *     tags={"Produtos"},
+ *     summary="Mostrar um produto",
+ *     @OA\Parameter(
+ *         description="ID do produto",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         @OA\Schema(type="integer"),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Produto Inexistente"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/search/namecategory/{name}/{category}",
+ *     tags={"Busca"},
+ *     summary="Buscar um produto por nome e categoria",
+ *     @OA\Parameter(
+ *         description="Nome do produto",
+ *         in="path",
+ *         name="name",
+ *         required=true,
+ *         @OA\Schema(type="string"),
+ *     ),
+ *     @OA\Parameter(
+ *         description="Categoria do produto",
+ *         in="path",
+ *         name="category",
+ *         required=true,
+ *         @OA\Schema(type="string"),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Nenhum produto encontrado"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/search/category/{category}",
+ *     tags={"Busca"},
+ *     summary="Buscar um produto por categoria",
+ *     @OA\Parameter(
+ *         description="Categoria do produto",
+ *         in="path",
+ *         name="category",
+ *         required=true,
+ *         @OA\Schema(type="string"),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     ),
+ *     @OA\Response(
+ *         response=400,
+ *         description="Nenhum produto encontrado"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/search/withimage",
+ *     tags={"Busca"},
+ *     summary="Buscar produtos com imagem",
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ *
+ * @OA\Get(
+ *     path="/api/search/withoutimage",
+ *     tags={"Busca"},
+ *     summary="Buscar produtos sem imagem",
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ *
+ */
 class ProductController extends Controller
 {
     /**
